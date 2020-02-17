4e941708108c345f9f6f1a5aac9525348464dac4
dahdouh <karim.dahdouh.fr@gmail.com>
2020-02-17 12:30:24 +0100

    Spring Mysql

---
+++ springMySQL/src/main/java/com/spring/JpaMysql/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.spring.JpaMysql.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.spring.JpaMysql.model.User;
+
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
---
+++ springMySQL/src/test/java/com/example/springMySQL/SpringMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springMySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
